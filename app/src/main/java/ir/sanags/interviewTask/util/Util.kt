a80cb911c3fc55a9be19af4b35c5d7a40ecb30d1
MrEleven <mr.amirprogrammer@gmail.com>
2022-04-15 15:22:17 +0430

    check network connection

---
+++ app/src/main/java/ir/sanags/interviewTask/util/Util.kt
@@ -0,0 +1,28 @@
+package ir.sanags.interviewTask.util
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.net.NetworkInfo
+import android.os.Build
+
+
+fun Context.hasNetwork(): Boolean {
+    val connMgr = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        connMgr.activeNetwork?.let { network ->
+            connMgr.getNetworkCapabilities(network)?.let { capabilities ->
+                when {
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                    else -> false
+                }
+            } ?: false
+        } ?: false
+    } else {
+        val networkInfo: NetworkInfo? = connMgr.activeNetworkInfo
+        networkInfo?.isConnected == true
+    }
+}
+
